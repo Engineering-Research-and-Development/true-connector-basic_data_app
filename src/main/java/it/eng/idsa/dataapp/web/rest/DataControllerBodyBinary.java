8e9484422ece7eed57bcea8f6e98977c6d207d4e
Petar Crepulja <petar.crepulja@eng.it>
2021-09-14 10:27:41 +0200

    Change in responseMessageString

--- src/main/java/it/eng/idsa/dataapp/web/rest/DataControllerBodyBinary.java
+++ src/main/java/it/eng/idsa/dataapp/web/rest/DataControllerBodyBinary.java
@@ -69,7 +69,7 @@ public class DataControllerBodyBinary {
 				.withHeaderContent(headerResponse)
 				.withPayloadContent(responsePayload)
 				.build();
-		String responseMessageString = MultipartMessageProcessor.multipartMessagetoString(responseMessage, false);
+		String responseMessageString = MultipartMessageProcessor.multipartMessagetoString(responseMessage, false, Boolean.TRUE);
 		
 		Optional<String> boundary = MultipartMessageProcessor.getMessageBoundaryFromMessage(responseMessageString);
 		String contentType = "multipart/mixed; boundary=" + boundary.orElse("---aaa") + ";charset=UTF-8";
