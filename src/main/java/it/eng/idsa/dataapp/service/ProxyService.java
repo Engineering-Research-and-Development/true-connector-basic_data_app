891bd8b6c662d2505027edba177255d12cb5eb86
Igor Balog Eng <70636468+IgorBalog-Eng@users.noreply.github.com>
2021-12-02 15:22:27 +0100

    Logic for encoding payload (#28)
    * Logic for encoding payload
    
    added property to configure logic for encoding
    refactored code
    added javadoc
    
    * Improved value binding for new property

--- src/main/java/it/eng/idsa/dataapp/service/ProxyService.java
+++ src/main/java/it/eng/idsa/dataapp/service/ProxyService.java
@@ -8,8 +8,31 @@ import org.springframework.http.ResponseEntity;
 import it.eng.idsa.dataapp.domain.ProxyRequest;
 
 public interface ProxyService {
+	/**
+	 * Creates multipart-mixed request and sends it to Sender ECC
+	 * @param proxyRequest - contains message type, payload and other required data for flow
+	 * @param httpHeaders - original headers from request; will be forwarded as-is
+	 * @return
+	 * @throws URISyntaxException
+	 */
 	ResponseEntity<String> proxyMultipartMix(ProxyRequest proxyRequest, HttpHeaders httpHeaders) throws URISyntaxException;
+	
+	/**
+	 * Creates multipart-form request and sends it to Sender ECC
+	 * @param proxyRequest - contains message type, payload and other required data for flow
+	 * @param httpHeaders - original headers from request; will be forwarded as-is
+	 * @return
+	 * @throws URISyntaxException
+	 */
 	ResponseEntity<String> proxyMultipartForm(ProxyRequest proxyRequest, HttpHeaders httpHeaders) throws URISyntaxException;
+	
+	/**
+	 * Creates http-header request and sends it to Sender ECC
+	 * @param proxyRequest - contains message type, payload and other required data for flow
+	 * @param httpHeaders - original headers from request; will be forwarded as-is
+	 * @return
+	 * @throws URISyntaxException
+	 */
 	ResponseEntity<String> proxyHttpHeader(ProxyRequest proxyRequest, HttpHeaders httpHeaders) throws URISyntaxException;
 	
 	ResponseEntity<String> requestArtifact(ProxyRequest proxyRequest);
