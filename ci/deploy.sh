1e976f32a60ebdd30702b15b511608f9f83f594a
David Jovanovic <david.jovanovic@eng.it>
2022-12-21 14:54:16 +0100

    added newman cli

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+echo "Installing Newman CLI..."
+sudo npm set strict-ssl false
+sudo npm install -g newman@5.2.2
+newman --version
+echo "Newman installed, READY TO TEST..."
+
+echo "Downloading and Installing docker-compose..."
+sudo rm /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "docker-compose correctly installed"
+
+sudo docker system prune --volumes -f
+
+mkdir -p $HOME/.m2
+
+cp -f ./ci/.m2/settings/settings.xml  $HOME/.m2
+
 echo "Creating Docker Container from Data-App repo..."
 sudo docker build -f Dockerfile -t rdlabengpa/ids_be_data_app:develop .
 cd ..
