f7cf215e20fcb93083b222d9878dd01e5190a523
igor.balog <igor.balog@eng.it>
2022-12-26 15:06:48 +0100

    Reverted changes

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,6 +5,6 @@ sudo docker build -f Dockerfile -t rdlabengpa/ids_be_data_app:develop .
 cd ..
 echo "Data-App is ready"
 echo "Starting deployment to Docker Hub"
-#sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-#sudo docker push rdlabengpa/ids_be_data_app:develop
+sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+sudo docker push rdlabengpa/ids_be_data_app:develop
 echo "Data-App deployed to Docker Hub"
