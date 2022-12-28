963a92ef2ed7bed61740f03850dd657bd4d2498b
Marko Stojanovic <stojanovic1994@yahoo.com>
2022-12-28 11:32:59 +0100

    Add common methods in DataAppMessageHandler

--- src/main/java/it/eng/idsa/dataapp/handler/DataAppMessageHandler.java
+++ src/main/java/it/eng/idsa/dataapp/handler/DataAppMessageHandler.java
@@ -1,10 +1,24 @@
 package it.eng.idsa.dataapp.handler;
 
+import java.net.URI;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 
 import de.fraunhofer.iais.eis.Message;
 
-public abstract class DataAppMessageHandler<M extends Message> {
+public abstract class DataAppMessageHandler {
 
-	public abstract Map<String, Object> handleMessage(M message, Object payload);
+	@Value("${application.ecc.issuer.connector}")
+	private String issuerConnector;
+
+	public abstract Map<String, Object> handleMessage(Message message, Object payload);
+
+	protected URI whoIAm() {
+		return URI.create("http://auto-generated");
+	}
+
+	protected URI whoIAmEngRDProvider() {
+		return URI.create(issuerConnector);
+	}
 }
