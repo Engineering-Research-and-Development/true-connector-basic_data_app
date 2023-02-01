5feff86f90eebdc3d5dcbf0c0d6447d37c77e25d
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-02-01 11:44:28 +0100

    Remove unused import

--- src/main/java/it/eng/idsa/dataapp/configuration/WebSocketMessageStreamerConfig.java
+++ src/main/java/it/eng/idsa/dataapp/configuration/WebSocketMessageStreamerConfig.java
@@ -4,7 +4,6 @@ import it.eng.idsa.dataapp.web.rest.IncomingDataAppResourceOverWs;
 import it.eng.idsa.streamer.WebSocketServerManager;
 import it.eng.idsa.streamer.websocket.receiver.server.FileRecreatorBeanExecutor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,24 +17,25 @@ import org.springframework.context.annotation.Configuration;
 //        havingValue = "true",
 //        matchIfMissing = false)
 public class WebSocketMessageStreamerConfig {
-    @Value("${application.fileSenderPort}")
-    private int port;
+	@Value("${application.fileSenderPort}")
+	private int port;
 
-    @Bean
-    public FileRecreatorBeanExecutor fileRecreatorBeanExecutor() {
-        FileRecreatorBeanExecutor fileRecreatorBeanExecutor = WebSocketServerManager.fileRecreatorBeanExecutor();
-        fileRecreatorBeanExecutor.setPort(port); //optional default 9000
-        //fileRecreatorBeanExecutor.setKeystorePassword("ssl-server.jks"); //optional default classpath: ssl-server.jks
-        //fileRecreatorBeanExecutor.setKeystorePassword("password");
-        fileRecreatorBeanExecutor.setPath("/incoming-data-app/routerBodyBinary");
-        fileRecreatorBeanExecutor.trigger(1000); //default 1000 1ms
-        return fileRecreatorBeanExecutor;
-    }
+	@Bean
+	public FileRecreatorBeanExecutor fileRecreatorBeanExecutor() {
+		FileRecreatorBeanExecutor fileRecreatorBeanExecutor = WebSocketServerManager.fileRecreatorBeanExecutor();
+		fileRecreatorBeanExecutor.setPort(port); // optional default 9000
+		// fileRecreatorBeanExecutor.setKeystorePassword("ssl-server.jks"); //optional
+		// default classpath: ssl-server.jks
+		// fileRecreatorBeanExecutor.setKeystorePassword("password");
+		fileRecreatorBeanExecutor.setPath("/incoming-data-app/routerBodyBinary");
+		fileRecreatorBeanExecutor.trigger(1000); // default 1000 1ms
+		return fileRecreatorBeanExecutor;
+	}
 
-    @Bean
-    public IncomingDataAppResourceOverWs incomingDataAppResourceOverWs(){
-        IncomingDataAppResourceOverWs incomingDataAppResourceOverWs = new IncomingDataAppResourceOverWs();
-        WebSocketServerManager.getMessageWebSocketResponse().addPropertyChangeListener(incomingDataAppResourceOverWs);
-        return incomingDataAppResourceOverWs;
-    }
+	@Bean
+	public IncomingDataAppResourceOverWs incomingDataAppResourceOverWs() {
+		IncomingDataAppResourceOverWs incomingDataAppResourceOverWs = new IncomingDataAppResourceOverWs();
+		WebSocketServerManager.getMessageWebSocketResponse().addPropertyChangeListener(incomingDataAppResourceOverWs);
+		return incomingDataAppResourceOverWs;
+	}
 }
