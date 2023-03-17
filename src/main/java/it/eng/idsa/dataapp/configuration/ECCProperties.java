3e2c4bd4bc1b12836e24c58ba7806970595d76f9
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-03-17 17:28:56 +0100

    Add wssPort and wssSelfDescriptionPort

--- src/main/java/it/eng/idsa/dataapp/configuration/ECCProperties.java
+++ src/main/java/it/eng/idsa/dataapp/configuration/ECCProperties.java
@@ -9,86 +9,127 @@ public class ECCProperties {
 	private String protocol;
 	private String host;
 	private int port;
+	private int wssPort;
+	private int wssSelfDescriptionPort;
 	private String selfdescriptionContext;
 	private String mixContext;
 	private String formContext;
 	private String headerContext;
-	
+
 	private String brokerRegisterContext;
 	private String brokerUpdateContext;
 	private String brokerDeleteContext;
 	private String brokerPassivateContext;
 	private String brokerQuerryContext;
-	
+
 	public String getProtocol() {
 		return protocol;
 	}
+
 	public void setProtocol(String protocol) {
 		this.protocol = protocol;
 	}
+
 	public String getHost() {
 		return host;
 	}
+
 	public void setHost(String host) {
 		this.host = host;
 	}
+
 	public int getPort() {
 		return port;
 	}
+
 	public void setPort(int port) {
 		this.port = port;
 	}
+
+	public int getWssPort() {
+		return wssPort;
+	}
+
+	public void setWssPort(int wssPort) {
+		this.wssPort = wssPort;
+	}
+
+	public int getWssSelfDescriptionPort() {
+		return getWssPort() + 100;
+	}
+
+	public void setWssSelfDescriptionPort(int wssSelfDescriptionPort) {
+		this.wssSelfDescriptionPort = wssSelfDescriptionPort;
+	}
+
 	public String getSelfdescriptionContext() {
 		return selfdescriptionContext;
 	}
+
 	public void setSelfdescriptionContext(String selfdescriptionContext) {
 		this.selfdescriptionContext = selfdescriptionContext;
 	}
+
 	public String getMixContext() {
 		return mixContext;
 	}
+
 	public void setMixContext(String mixContext) {
 		this.mixContext = mixContext;
 	}
+
 	public String getFormContext() {
 		return formContext;
 	}
+
 	public void setFormContext(String formContext) {
 		this.formContext = formContext;
 	}
+
 	public String getHeaderContext() {
 		return headerContext;
 	}
+
 	public void setHeaderContext(String headerContext) {
 		this.headerContext = headerContext;
 	}
+
 	public String getBrokerRegisterContext() {
 		return brokerRegisterContext;
 	}
+
 	public void setBrokerRegisterContext(String brokerRegisterContext) {
 		this.brokerRegisterContext = brokerRegisterContext;
 	}
+
 	public String getBrokerUpdateContext() {
 		return brokerUpdateContext;
 	}
+
 	public void setBrokerUpdateContext(String brokerUpdateContext) {
 		this.brokerUpdateContext = brokerUpdateContext;
 	}
+
 	public String getBrokerDeleteContext() {
 		return brokerDeleteContext;
 	}
+
 	public void setBrokerDeleteContext(String brokerDeleteContext) {
 		this.brokerDeleteContext = brokerDeleteContext;
 	}
+
 	public String getBrokerPassivateContext() {
 		return brokerPassivateContext;
 	}
+
 	public void setBrokerPassivateContext(String brokerPassivateContext) {
 		this.brokerPassivateContext = brokerPassivateContext;
 	}
+
 	public String getBrokerQuerryContext() {
 		return brokerQuerryContext;
 	}
+
 	public void setBrokerQuerryContext(String brokerQuerryContext) {
 		this.brokerQuerryContext = brokerQuerryContext;
 	}
