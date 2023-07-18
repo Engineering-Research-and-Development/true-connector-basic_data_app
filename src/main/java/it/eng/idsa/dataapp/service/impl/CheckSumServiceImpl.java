360c7c0731cc820601b85869ba6ee30c3d277dcd
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-07-18 12:25:14 +0200

    Modify CheckSumServiceImpl to be conditional and use repositories

--- src/main/java/it/eng/idsa/dataapp/service/impl/CheckSumServiceImpl.java
+++ src/main/java/it/eng/idsa/dataapp/service/impl/CheckSumServiceImpl.java
@@ -1,60 +1,46 @@
 package it.eng.idsa.dataapp.service.impl;
 
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.zip.CRC32C;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Service;
 
+import it.eng.idsa.dataapp.repository.CheckSumRepository;
 import it.eng.idsa.dataapp.service.CheckSumService;
 
 @Service
+@ConditionalOnExpression("'${application.verifyCheckSum}' == 'true'")
 public class CheckSumServiceImpl implements CheckSumService {
 
 	private static final Logger logger = LoggerFactory.getLogger(CheckSumService.class);
 
-	@Value("${application.checkSumStorage}")
-	private String checkSumStorage;
+	private final CheckSumRepository checkSumRepository;
 
-	private final ConcurrentHashMap<String, Object> inMemoryStorage;
-
-	public CheckSumServiceImpl(ConcurrentHashMap<String, Object> inMemoryStorage) {
-		this.inMemoryStorage = inMemoryStorage;
+	public CheckSumServiceImpl(CheckSumRepository checkSumRepository) {
+		this.checkSumRepository = checkSumRepository;
 	}
 
 	@Override
 	public void addCheckSum(String targetArtifact, Long value) {
-		if (StringUtils.equals(checkSumStorage, "h2")) {
-			logger.debug("Using h2 as persistence");
-		} else {
-			logger.debug("Using threadLocal as persistence");
-			addNewCheckSum(targetArtifact, value);
-		}
+		logger.info("Adding checkSum to storage...");
+		checkSumRepository.save(targetArtifact, value);
+
 	}
 
 	@Override
 	public Long getCheckSumByArtifactId(String targetArtifact) {
-		if (StringUtils.equals(checkSumStorage, "h2")) {
-			logger.debug("Using h2 as persistence");
-			return null;
-		} else {
-			logger.debug("Using threadLocal as persistence");
-			return getCheckSumFromStorageByTarget(targetArtifact);
-		}
+		logger.info("Getting checkSum from storage...");
+
+		return checkSumRepository.getByArtifactId(targetArtifact);
 	}
 
 	@Override
 	public void deleteCheckSumByArtifactId(String targetArtifact) {
-		if (StringUtils.equals(checkSumStorage, "h2")) {
-			logger.debug("Using h2 as persistence");
-		} else {
-			logger.debug("Using threadLocal as persistence");
+		logger.info("Deleting checkSum from storage...");
 
-			inMemoryStorage.remove(targetArtifact);
-		}
+		checkSumRepository.delete(targetArtifact);
 	}
 
 	@Override
@@ -67,14 +53,6 @@ public class CheckSumServiceImpl implements CheckSumService {
 		final var checksum = new CRC32C();
 		checksum.update(bytes, 0, bytes.length);
 		return checksum.getValue();
-	}
-
-	private Long getCheckSumFromStorageByTarget(String targetArtifact) {
-		return (Long) inMemoryStorage.get(targetArtifact);
-	}
-
-	private void addNewCheckSum(String targetArtifact, Long value) {
-		inMemoryStorage.put(targetArtifact, value);
 	}
 
 }
