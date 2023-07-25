caa84152de4cce8f401d4cf2e99a9c76d4ec803c
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-07-25 10:43:00 +0200

    Set user credentials to be fixed

--- src/main/java/it/eng/idsa/dataapp/configuration/SecurityConfig.java
+++ src/main/java/it/eng/idsa/dataapp/configuration/SecurityConfig.java
@@ -1,6 +1,5 @@
 package it.eng.idsa.dataapp.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,36 +14,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Value("${application.security.username}")
-	private String username;
-	@Value("${application.security.password}")
-	private String password;
+	private static final String USERNAME = "proxy";
+
+	private static final String PASSWORD = "$2a$10$MQ5grDaIqDpBjMlG78PFduv.AMRe9cs0CNm/V4cgUubrqdGTFCH3m";
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser(username).password(password).roles("PROXY");
+		auth.inMemoryAuthentication().withUser(USERNAME).password(PASSWORD).roles("PROXY");
 	}
-	
+
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            //HTTP Basic authentication
-        .httpBasic()
-        .and()
-        .sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-        .authorizeRequests()
-        .antMatchers("/proxy").hasRole("PROXY")
-        .antMatchers("/about/**").permitAll()
-		.antMatchers("/error").permitAll()
-		.antMatchers("/data").permitAll()
-		.antMatchers("/incoming-data-app/routerBodyBinary").permitAll()
-        .and()
-        .csrf().disable()
-        .formLogin().disable();
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				// HTTP Basic authentication
+				.httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeRequests().antMatchers("/proxy").hasRole("PROXY").antMatchers("/about/**").permitAll()
+				.antMatchers("/error").permitAll().antMatchers("/data").permitAll()
+				.antMatchers("/incoming-data-app/routerBodyBinary").permitAll().and().csrf().disable().formLogin()
+				.disable();
 	}
-	
+
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
