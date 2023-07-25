200e3bd6277770c0ebd6996d740773ee79bf1c34
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-07-25 11:08:13 +0200

    Revert password as configurable property

--- src/main/java/it/eng/idsa/dataapp/configuration/SecurityConfig.java
+++ src/main/java/it/eng/idsa/dataapp/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package it.eng.idsa.dataapp.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,25 +16,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String USERNAME = "proxy";
-
-	private static final String PASSWORD = "$2a$10$MQ5grDaIqDpBjMlG78PFduv.AMRe9cs0CNm/V4cgUubrqdGTFCH3m";
+	@Value("${application.security.password}")
+	private String password;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser(USERNAME).password(PASSWORD).roles("PROXY");
+		auth.inMemoryAuthentication().withUser(USERNAME).password(password).roles("PROXY");
 	}
-
+	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				// HTTP Basic authentication
-				.httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.authorizeRequests().antMatchers("/proxy").hasRole("PROXY").antMatchers("/about/**").permitAll()
-				.antMatchers("/error").permitAll().antMatchers("/data").permitAll()
-				.antMatchers("/incoming-data-app/routerBodyBinary").permitAll().and().csrf().disable().formLogin()
-				.disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            //HTTP Basic authentication
+        .httpBasic()
+        .and()
+        .sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+        .authorizeRequests()
+        .antMatchers("/proxy").hasRole("PROXY")
+        .antMatchers("/about/**").permitAll()
+		.antMatchers("/error").permitAll()
+		.antMatchers("/data").permitAll()
+		.antMatchers("/incoming-data-app/routerBodyBinary").permitAll()
+        .and()
+        .csrf().disable()
+        .formLogin().disable();
 	}
-
+	
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
