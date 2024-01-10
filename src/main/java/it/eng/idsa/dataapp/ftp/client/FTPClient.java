c57969fbac13b4eb847270059d93345851d3a137
Marko Stojanovic <stojanovic1994@yahoo.com>
2024-01-10 15:42:40 +0100

    Refactor FTPClient.java to be multithread compliant

--- src/main/java/it/eng/idsa/dataapp/ftp/client/FTPClient.java
+++ src/main/java/it/eng/idsa/dataapp/ftp/client/FTPClient.java
@@ -1,15 +1,11 @@
 package it.eng.idsa.dataapp.ftp.client;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.concurrent.TimeUnit;
-
-import javax.annotation.PreDestroy;
 
 import org.apache.sshd.client.SshClient;
 import org.apache.sshd.client.keyverifier.RequiredServerKeyVerifier;
@@ -53,21 +49,6 @@ public class FTPClient {
 	@Autowired
 	private KeyStoreConfiguration keyStoreConfiguration;
 
-	SshClient client;
-	ClientSession clientSession;
-
-	private void getClientSession() throws URISyntaxException, IOException {
-		client = SshClient.setUpDefaultClient();
-
-		client.addPublicKeyIdentity(loadKeyPair());
-		client.addPasswordIdentity(tlsKeystorePassword);
-
-		client.setServerKeyVerifier(new RequiredServerKeyVerifier(keyStoreConfiguration.getPublicKey()));
-		client.setKeyIdentityProvider(KeyIdentityProvider.wrapKeyPairs(
-				new KeyPair(keyStoreConfiguration.getPublicKey(), keyStoreConfiguration.getPrivateKey())));
-		client.start();
-	}
-
 	private KeyPair loadKeyPair() {
 		logger.info("Creating keypair on FTP Client side");
 		PublicKey publicKey = keyStoreConfiguration.getPublicKey();
@@ -77,11 +58,16 @@ public class FTPClient {
 	}
 
 	public boolean downloadArtifact(String artifact) throws Exception {
-		getClientSession();
-		boolean result = false;
+		SshClient client = SshClient.setUpDefaultClient();
+		client.addPublicKeyIdentity(loadKeyPair());
+		client.addPasswordIdentity(tlsKeystorePassword);
+		client.setServerKeyVerifier(new RequiredServerKeyVerifier(keyStoreConfiguration.getPublicKey()));
+		client.setKeyIdentityProvider(KeyIdentityProvider.wrapKeyPairs(
+				new KeyPair(keyStoreConfiguration.getPublicKey(), keyStoreConfiguration.getPrivateKey())));
+		client.start();
+
 		try (ClientSession session = client.connect(username, host, port)
 				.verify(defaultTimeoutSeconds, TimeUnit.SECONDS).getSession()) {
-
 			session.addPublicKeyIdentity(loadKeyPair());
 			session.auth().verify(defaultTimeoutSeconds, TimeUnit.SECONDS);
 
@@ -89,25 +75,24 @@ public class FTPClient {
 					ByteArrayOutputStream errorResponseStream = new ByteArrayOutputStream()) {
 
 				ScpClient scpClient = DefaultScpClientCreator.INSTANCE.createScpClient(session);
-				scpClient.download(artifact, dataLakeDirectory, ScpClient.Option.PreserveAttributes,
+				scpClient.download(artifact, "/home/mare/wss_test", ScpClient.Option.PreserveAttributes,
 						ScpClient.Option.TargetIsDirectory);
 
 				String errorString = new String(errorResponseStream.toByteArray(), StandardCharsets.UTF_8);
 				if (!errorString.isEmpty()) {
-					logger.error("Following error occured: ", errorString);
+					logger.error("Following error occurred: ", errorString);
 					throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
 							"Error while processing request, check logs for more details" + errorString);
 				}
-				result = true;
+				return true;
+			}
+		} catch (Exception e) {
+			logger.error("Error in downloadArtifact: ", e);
+			throw e;
+		} finally {
+			if (client != null) {
 				client.stop();
 			}
 		}
-		return result;
-	}
-
-	@PreDestroy
-	public void shutdown() {
-		logger.info("Shutting down client");
-		client.stop();
 	}
 }
