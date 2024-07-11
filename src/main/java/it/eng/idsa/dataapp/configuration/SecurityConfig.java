e9f52f455dc7e0fbab49ea11df1ccbf2219c7a53
Marko <stojanovic1994@yahoo.com>
2024-07-11 16:06:58 +0200

    Feature/cors (#138)
    * Delete .m2 dependencies
    
    * Improve handling attributes in firewall
    
    * Add CORS configuration
    
    * Update pom.xml to use updated FH repo and latest MMP and WMS
    
    * Update GHA
    
    * Update CHANGELOG.md
    
    * Modify SecurityConfig to use CORS config from property file
    
    * Update properties files
    
    * Update CHANGELOG.md and README.md

--- src/main/java/it/eng/idsa/dataapp/configuration/SecurityConfig.java
+++ src/main/java/it/eng/idsa/dataapp/configuration/SecurityConfig.java
@@ -1,7 +1,11 @@
 package it.eng.idsa.dataapp.configuration;
 
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,12 +15,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
+	@Value("${application.cors.allowed.origins:}")
+	private String allowedOrigins;
+
+	@Value("${application.cors.allowed.methods:}")
+	private String allowedMethods;
+
+	@Value("${application.cors.allowed.headers:}")
+	private String allowedHeaders;
 
 	private final UserProperties userProperties;
 
@@ -40,17 +56,46 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-				// HTTP Basic authentication
-				.httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.authorizeRequests().antMatchers("/proxy").hasRole("PROXY").antMatchers("/about/**").permitAll()
-				.antMatchers("/error").permitAll().antMatchers("/data").permitAll()
-				.antMatchers("/incoming-data-app/routerBodyBinary").permitAll().and().csrf().disable().formLogin()
-				.disable().headers().xssProtection().and().contentTypeOptions().and().frameOptions().sameOrigin();
+		http.cors().and()
+		.csrf().disable().httpBasic().and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeRequests().antMatchers("/proxy")
+		.hasRole("PROXY").antMatchers("/about/**").permitAll().antMatchers("/error").permitAll()
+		.antMatchers("/data").permitAll().antMatchers("/incoming-data-app/routerBodyBinary").permitAll().and()
+		.formLogin().disable().headers().xssProtection().and().contentTypeOptions().and().frameOptions()
+		.sameOrigin();
 	}
 
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		if (StringUtils.isBlank(allowedOrigins)) {
+			configuration.addAllowedOrigin("*");
+		} else {
+			configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+		}
+
+		if (StringUtils.isBlank(allowedMethods)) {
+			configuration.addAllowedMethod("*");
+		} else {
+			configuration.setAllowedMethods(Arrays.asList(allowedMethods.split(",")));
+		}
+
+		if (StringUtils.isBlank(allowedHeaders)) {
+			configuration.addAllowedHeader("*");
+		} else {
+			configuration.setAllowedHeaders(Arrays.asList(allowedHeaders.split(",")));
+		}
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
